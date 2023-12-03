edb5bbe03f2393336d93a3e690aa10d29bf8aa6a
BEULAHEVANJALIN <beulahebenezer777@gmail.com>
2023-12-03 20:59:33 +0530

    Initial commit; --- Day 1: Trebuchet?! ---

---
+++ src/Day1Trebuchet.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Day1Trebuchet {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://adventofcode.com/2023/day/1/input");
+            HttpURLConnection connection = getHttpURLConnection(url);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            int result = 0;
+            while ((line = reader.readLine()) != null) {
+                result += extractAndParseInt(line);
+            }
+            System.out.println(result);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int extractAndParseInt(String line) {
+        String firstDigit = "";
+        String lastDigit = "";
+        for (char ch : line.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                if (firstDigit.isEmpty()) {
+                    firstDigit = String.valueOf(ch);
+                }
+                lastDigit = String.valueOf(ch);
+            }
+        }
+        try {
+            return firstDigit.isEmpty() ? 0 : Integer.parseInt(firstDigit + lastDigit);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    private static HttpURLConnection getHttpURLConnection(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+        connection.setRequestProperty("Cookie", "YOUR_SESSION_COOKIE");
+        return connection;
+    }
+}
